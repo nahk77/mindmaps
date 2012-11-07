fec0d992d5592e5b4e2f80ade8dba46f34a885dd
Hanno Braun <mail@hannobraun.com>
2012-11-07 11:20:32 +0100

    Fixed a bug that prevented displaying the line to a node while it is created.

--- src/js/CanvasDrawingTools.js
+++ src/js/CanvasDrawingTools.js
@@ -9,7 +9,12 @@ mindmaps.CanvasDrawingUtil = {
     // var width = this.zoomFactor * (10 - depth);
     var width = zoomFactor * (12 - depth * 2);
 
-    width += $node.data().node.getLineWidthOffset();
+    var offset = 0
+    if ($node.data().node !== undefined) {
+      offset = $node.data().node.getLineWidthOffset();
+    }
+
+    width += offset
 
     if (width < 2) {
       width = 2;
