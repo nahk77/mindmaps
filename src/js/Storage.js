8a08d693d7fbb7720a867bb924b0f06633e2abb2
nahk77 <nahk77@yahoo.com>
2013-01-24 11:34:35 +0100

    Update src/js/Storage.js
    Functions to save to file (in debug mode),
     create/save tree/summary of mindmap
    encode all text in mindmap.

--- src/js/Storage.js
+++ src/js/Storage.js
@@ -68,7 +68,62 @@ mindmaps.LocalDocumentStorage = (function() {
      */
     saveDocument : function(doc) {
       try {
-        localStorage.setItem(prefix + doc.id, doc.serialize());
+    if(document.getElementById('myOptions_local').checked) {
+  	     localStorage.setItem(prefix + doc.id, doc.serialize()); //this is the original line, save to localStorage
+		}else if(document.getElementById('myOptions_file').checked) {
+	    var mmm  ="";
+		  if ( document.getElementById('myOptions_encodeKey').value !=""){//encoding key present
+			  var hash = 0;
+			  for (var i = 0; i <document.getElementById('myOptions_encodeKey').value.length; i++) {
+			  	hash = document.getElementById('myOptions_encodeKey').value.charCodeAt(i) + ((hash << 5) - hash);
+			  }
+			  mmm = doc.serialize().replace(/caption":"(.*?)","font/g, function(a, b) {  
+			      var to_enc = b;
+			      var xor_key=document.getElementById('myOptions_encodeKey').value ;
+			      var the_res="";//the result will be here
+			      for(i=0;i<to_enc.length;++i)
+			        {
+				       the_res+=String.fromCharCode(xor_key^to_enc.charCodeAt(i));
+			        }
+			      return ('caption":"'+ the_res+'","font');
+			    }) ;
+		  } else {   mmm  =doc.serialize(); } //no encoding key present
+		  uriContent = "data:application/octet-stream," + encodeURIComponent(mmm); 
+		  alert (doc["mindmap"].getRoot().getCaption().toString()+".json" );	newWindow=window.open(uriContent, 'neuesDokument');
+	  }else if( (document.getElementById('myOptions_createPresentationVisible').checked) || (document.getElementById('myOptions_createPresentation').checked) || (document.getElementById('myOptions_codePresentation').checked)) {
+	    var slideId = 1;
+		  var myPresentation = '<article style="left: 100px;" class="activeSlide" id="'+ slideId +'" contenteditable="true"> <center><h1><br><br><br>' + doc.title+'</h1>  </center>  ...  </article>	' ;
+		  slideId += 1;
+	 	  if(doc.mindmap.root.children) {
+				for ( i in doc.mindmap.root.children.nodes) {
+					myPresentation += '<hr width="50%" style="color:'+ doc.mindmap.root.children.nodes[i].branchColor+'"  ><article style="left: 100px;" class="activeSlide" id="'+ slideId +'" contenteditable="true">  <h1 style="color:'+ doc.mindmap.root.children.nodes[i].text.font.color +';font-style:'+doc.mindmap.root.children.nodes[i].text.font.style +';text-decoration:'+ doc.mindmap.root.children.nodes[i].text.font.decoration+';font-weight:' +doc.mindmap.root.children.nodes[i].text.font.weight+';"> ' + doc.mindmap.root.children.nodes[i].text.caption+'</h1>    	' ;
+					var theBranch =  doc.mindmap.root.children.nodes[i];
+					(function foo (theBranch) {
+							var myChildren = []; 
+							for (c in theBranch.children.nodes) myChildren.push(theBranch.children.nodes[c].text.caption);
+							if (myChildren.length != 0) {
+								if (  (document.getElementById('myOptions_createPresentationVisible').checked == 0 )  || ((document.getElementById('myOptions_createPresentationVisible').checked)  && (theBranch.foldChildren== 0 )) ){
+								myPresentation += '<ul>' ;
+								for (c in theBranch.children.nodes) {myPresentation += '<li style="color:'+ theBranch.children.nodes[c].branchColor +'"   ><span style="color:'+ theBranch.children.nodes[c].text.font.color +';font-style:'+theBranch.children.nodes[c].text.font.style +';text-decoration:'+ theBranch.children.nodes[c].text.font.decoration+';font-weight:' +theBranch.children.nodes[c].text.font.weight+';" >' +theBranch.children.nodes[c].text.caption+'</span></li>'  ; foo(theBranch.children.nodes[c]);  }
+								myPresentation += '</ul>';
+								} 
+							}
+					}(  doc.mindmap.root.children.nodes[i]));
+		
+					myPresentation += '   </article>';
+					slideId += 1;
+				}
+		} 
+		 
+		if ( (document.getElementById('myOptions_createPresentation').checked) || (document.getElementById('myOptions_createPresentationVisible').checked) ){
+			newWindow=window.open('', 'presentation');
+			newWindow.document.write( '<html><body>'+myPresentation + '</body></html>');
+		}else if  (document.getElementById('myOptions_codePresentation').checked) {
+		 uriContent = "data:application/octet-stream," + encodeURIComponent('<html><body>'+myPresentation + '</body></html>'); 
+			alert (doc["mindmap"].getRoot().getCaption().toString()+".json" );	newWindow=window.open(uriContent, 'neuesDokument');
+		}
+		
+	}
         return true;
       } catch (error) {
         // QUOTA_EXCEEDED
